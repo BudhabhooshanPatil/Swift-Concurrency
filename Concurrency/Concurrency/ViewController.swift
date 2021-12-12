05a58eb3e5c06dfcad161da40fe5aa9851df58ef
Bhooshan Patil <budhabhooshan.patil@coditas.com>
2021-12-12 17:51:35 +0530

    added async-await demo-app

---
+++ Concurrency/Concurrency/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Concurrency
+//
+//  Created by Bhooshan Patil on 10/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let url = "https://jsonplaceholder.typicode.com/users"
+    
+    // Sync
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("before async ")
+        Task {
+            await asynCaller()
+        }
+        Thread.isMainThread ? print("Main Thread") : print("not on main thread")
+        print("after async")
+    }
+    
+    // Asyc
+    private func asynCaller() async {
+        print("inside async calling async")
+        Thread.isMainThread ? print("Main Thread") : print("not on main thread")
+        do {
+            let _ = try await downloadTask()
+        } catch  {
+            print(error)
+        }
+    }
+    
+    // Async
+    private func downloadTask() async throws -> (Data, URLResponse) {
+        someSyncFunc()
+        Thread.isMainThread ? print("Main Thread") : print("not on main thread")
+        let requestURL = URL(string: url)!
+        let request = URLRequest(url: requestURL)
+        do {
+            let response = try await URLSession.shared.data(for: request)
+            return response
+        } catch {
+            throw error
+        }
+    }
+    
+    // Sync
+    private func someSyncFunc() {
+        print("inside async calling sync")
+        Thread.isMainThread ? print("Main Thread") : print("not on main thread")
+    }
+}
+
